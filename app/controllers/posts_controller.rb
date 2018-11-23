2e727d0b7ea4fab0b50fab94d4764783b250c29e
Zion <bone2057@gmail.com>
2018-11-23 19:41:33 +0800

    only group member can create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,14 @@
 class PostsController < ApplicationController
+  before_action :find_group
   before_action :authenticate_user!, :only => [:new, :create]
+  before_action :member_required, :only => [:new, :create ]
 
   def new
-    @group = Group.find(params[:group_id])
-    @post = Post.new
+    @post = @group.posts.build
   end
 
   def create
-    @group = Group.find(params[:group_id])
-    @post = Post.new(post_params)
-    @post.group = @group
+    @post = @group.posts.new(post_params)
     @post.user = current_user
     if @post.save
       redirect_to group_path(@group)
@@ -20,12 +19,12 @@ class PostsController < ApplicationController
 
   def edit
     @group = Group.find(params[:group_id])
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
+
   end
 
   def update
-    @group = Group.find(params[:group_id])
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
     if @post.update(post_params)
       redirect_to account_posts_path, notice: "Update Success"
     else
@@ -34,7 +33,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:group_id])
+    @post = current_user.posts.find(params[:id])
     @post = Post.find(params[:id])
     @post.destroy
     flash[:alert] = "Group deleted"
@@ -45,6 +44,17 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def member_required
+    if !current_user.is_member_of?(@group)
+      flash[:warning] = " You are not member of this group!"
+      redirect_to group_path(@group)
+    end
+  end
+
+  def find_group
+    @group = Group.find(params[:group_id])
   end
 
 end
